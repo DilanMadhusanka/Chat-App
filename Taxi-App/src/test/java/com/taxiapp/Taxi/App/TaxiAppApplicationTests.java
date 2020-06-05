68399bc8d3466760a34dff97a37e8e8fddedf618
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-06-05 23:02:28 +0530

    generate spring boot package and import dependencies

---
+++ Taxi-App/src/test/java/com/taxiapp/Taxi/App/TaxiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taxiapp.Taxi.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxiAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
