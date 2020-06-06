fbcc3935e2ec0a8958b49f9a70f5ff3c750d7093
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-06-06 08:50:33 +0530

    create services class

--- Taxi-App/src/main/java/com/taxiapp/Taxi/App/repository/UserRepository.java
+++ Taxi-App/src/main/java/com/taxiapp/Taxi/App/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.taxiapp.Taxi.App.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.taxiapp.Taxi.App.models.User;
 
 public interface UserRepository extends MongoRepository<User, String>{
+
+	Optional<User> findByUsername(String username);
 
 }
