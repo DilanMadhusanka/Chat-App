c5134e413fd2f51537dc9684723874148452ee3d
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-06-06 12:03:11 +0530

    create AuthController class

--- Taxi-App/src/main/java/com/taxiapp/Taxi/App/repository/RoleRepository.java
+++ Taxi-App/src/main/java/com/taxiapp/Taxi/App/repository/RoleRepository.java
@@ -1,9 +1,14 @@
 package com.taxiapp.Taxi.App.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import com.taxiapp.Taxi.App.models.ERole;
 import com.taxiapp.Taxi.App.models.Role;
 
 public interface RoleRepository extends MongoRepository<Role, String>{
+
+	Optional<Role> findByName(ERole name);
 
 }
--- Taxi-App/src/main/java/com/taxiapp/Taxi/App/repository/UserRepository.java
+++ Taxi-App/src/main/java/com/taxiapp/Taxi/App/repository/UserRepository.java
@@ -10,4 +10,8 @@ public interface UserRepository extends MongoRepository<User, String>{
 
 	Optional<User> findByUsername(String username);
 
+	boolean existsByUsername(String username);
+
+	boolean existsByEmail(String email);
+
 }
