455bf7743ce427811fc2cc7d2ba820c03133ab3a
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-06-06 11:39:16 +0530

    create WebSecurityConfig configure file

---
+++ Taxi-App/src/main/java/com/taxiapp/Taxi/App/payload/response/MessageResponse.java
@@ -0,0 +1,18 @@
+package com.taxiapp.Taxi.App.payload.response;
+
+public class MessageResponse {
+
+	private String message;
+
+	public MessageResponse(String message) {
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
