005d09bd02aeb6ea03d28017fd146a226ecfe387
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-06-07 23:21:11 +0530

    use @EnableEurekaClient and set the name to the eureka client and add neccessary port 8761 for eureka server

--- Taxi-App/src/main/java/com/taxiapp/Taxi/App/TaxiAppApplication.java
+++ Taxi-App/src/main/java/com/taxiapp/Taxi/App/TaxiAppApplication.java
@@ -2,8 +2,10 @@ package com.taxiapp.Taxi.App;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class TaxiAppApplication {
 
 	public static void main(String[] args) {
