e6d36ac25ce8964c25d8aefcd6fd0f2f6c807487
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-06-07 21:02:56 +0530

    generate springboot project for eureka server

---
+++ eureka-server/src/test/java/com/taxiapp/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taxiapp.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
