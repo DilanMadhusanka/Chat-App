f2dc5baa47f60265464b7750e8a5ec4830e0dfdf
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-06-08 10:11:09 +0530

    generate maven project for chat box as eureka client

---
+++ chat-box/src/main/java/com/taxiapp/chatbox/ChatBoxEurekaClientApplication.java
@@ -0,0 +1,13 @@
+package com.taxiapp.chatbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatBoxEurekaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatBoxEurekaClientApplication.class, args);
+	}
+
+}
