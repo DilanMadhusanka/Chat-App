4474dec8b38d1f4874dad0cb28942d629520e256
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-07-28 19:50:39 +0530

    import the ui components to App component and create Routes

--- chat-app-client/src/App.js
+++ chat-app-client/src/App.js
@@ -3,17 +3,46 @@ import ChatMessageBox from './components/ChatMessageBox/ChatMessageBox'
 // Re-using my ErrorBoundary Component 
 // import ErrorBoundary from 'react-error-boundary';
 
+import React from 'react';
+import LoginComponent from './components/LoginComponent';
+import { Router, Switch, Route } from 'react-router-dom';
+import Home from './Home';
+import NavBar from './components/NavBar';
+import Profile from './Profile';
+import history from '../history'
+import BoardUser from './components/user-board.component';
+import BoardModerator from './components/moderator-board.component'
+import BoardAdmin from './components/admin-board.component'
+import SignUpComponent from './components/SignUpComponent';
+
 class App extends React.Component {
 
   refreshPage() {
     window.location.reload();
   }
 
+  // render() {
+  //   return (
+  //     <ChatMessageBox/>
+  //   )
+  // }
+
   render() {
-    return (
-      <ChatMessageBox/>
-    )
-  }
+    return <div>
+        <Router history={history}>
+            <NavBar />
+            <Switch>
+                <Route exact path={["/", "/home"]} component={Home} />
+                <Route exact path={"/login"} component={LoginComponent} />
+                <Route exact path={"/profile"} component={Profile} />
+                <Route exact path={"/user"} component={BoardUser} />
+                <Route exact path={"/moderator"} component={BoardModerator} />
+                <Route exact path={"/admin"} component={BoardAdmin} />
+                <Route exact path={"/signup"} component={SignUpComponent} />
+            </Switch>
+        </Router>
+    </div>
+}
 }
 
 export default App;
