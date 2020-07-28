c4976221191770df71b2fcdd5cd545a4f70f21ba
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-07-28 22:11:12 +0530

    create Route for chat room

--- chat-app-client/src/App.js
+++ chat-app-client/src/App.js
@@ -13,6 +13,7 @@ import BoardUser from './components/user-board.component';
 import BoardModerator from './components/moderator-board.component'
 import BoardAdmin from './components/admin-board.component'
 import SignUpComponent from './components/SignUpComponent';
+import ChatMessageBox from './components/ChatMessageBox/ChatMessageBox';
 
 class App extends React.Component {
 
@@ -28,20 +29,21 @@ class App extends React.Component {
 
   render() {
     return <div>
-        <Router history={history}>
-            <NavBar />
-            <Switch>
-                <Route exact path={["/", "/home"]} component={Home} />
-                <Route exact path={"/login"} component={LoginComponent} />
-                <Route exact path={"/profile"} component={Profile} />
-                <Route exact path={"/user"} component={BoardUser} />
-                <Route exact path={"/moderator"} component={BoardModerator} />
-                <Route exact path={"/admin"} component={BoardAdmin} />
-                <Route exact path={"/signup"} component={SignUpComponent} />
-            </Switch>
-        </Router>
+      <Router history={history}>
+        <NavBar />
+        <Switch>
+          <Route exact path={["/", "/home"]} component={Home} />
+          <Route exact path={"/login"} component={LoginComponent} />
+          <Route exact path={"/profile"} component={Profile} />
+          <Route exact path={"/user"} component={BoardUser} />
+          <Route exact path={"/moderator"} component={BoardModerator} />
+          <Route exact path={"/admin"} component={BoardAdmin} />
+          <Route exact path={"/signup"} component={SignUpComponent} />
+          <Route exact path={"/chat"} component={ChatMessageBox} />
+        </Switch>
+      </Router>
     </div>
-}
+  }
 }
 
 export default App;
