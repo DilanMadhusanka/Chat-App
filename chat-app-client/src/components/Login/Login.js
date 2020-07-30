35efb0e3de5061cb98f59835963c84ebfe4b01d6
Dilan madhusanka <37908400+DilanMadhusanka@users.noreply.github.com>
2020-07-30 23:13:22 +0530

    remove the blue background color

--- chat-app-client/src/components/Login/Login.js
+++ chat-app-client/src/components/Login/Login.js
@@ -1,43 +1,13 @@
 import React from 'react';
-import { TextField, Button, Card } from '@material-ui/core';
+import { TextField, Button, Card, CircularProgress } from '@material-ui/core';
 import './Login.css';
 
 export default class Login extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state =
-        {
-            username: ''
-        };
-    }
-
-    handleUserNameChange = (event) => {
-        this.setState({
-            username: event.target.value,
-        });
-    };
-
-    handleConnectPublicly = () => {
-        this.props.connect(this.state.username, false)
-    }
 
     render() {
         return (
             <div className="ui center aligned container">
-                <br/><br/><br/>
-                <Card className="ui segment" variant="outlined">
-                    <TextField
-                        id="user"
-                        label="Type your username"
-                        placeholder="Username"
-                        onChange={this.handleUserNameChange}
-                        margin="normal"
-                    />
-                    <br />
-                    <Button variant="contained" color="primary" onClick={this.handleConnectPublicly} >
-                        Start Chatting
-             </Button>
-                </Card>
+                <CircularProgress color="inherit" />
 
             </div>
         )
